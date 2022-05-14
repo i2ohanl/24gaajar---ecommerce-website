37aa8e9427760b855ae8ae6d6843cefec73c1cc7
i2ohanl <77062490+i2ohanl@users.noreply.github.com>
2022-05-14 06:55:34 +0530

    Add files via upload

---
+++ 24Gaajar-main/README.md
@@ -0,0 +1,15 @@
+# 24Gaajar 
+This is a grocery delivery app created with the help of MERN stack - MongoDB, ExpressJS, ReactJS, NodeJS.
+
+# To run
+
+1.) npx create-react-app 24Gaajar
+or
+npm init react-app 24Gaajar
+
+2.) cd 24gaajar
+
+3.) npm run dev
+
+
+
---
+++ 24Gaajar-main/backend/data/products.js
@@ -0,0 +1,58 @@
+const products = [
+    {
+      name: "Aloo",
+      imageUrl:
+       "https://cflare.shop.bigbazaar.com/cdn-cgi/image/width=450/media/product/59/04/7-wide_zoom.jpg",
+      description:
+        "Contain antioxidants,May improve digestive health, May improve blood sugar control,Contain significant amounts of fiber",
+      price: 40,
+      countInStock: 15,
+    },
+    {
+      name: "Onion",
+      imageUrl:
+        "https://cflare.shop.bigbazaar.com/cdn-cgi/image/width=450/media/product/09/6401/11-wide_zoom.jpg",
+      description:
+        "Onions are richer in antioxidants than other. High in Vitamin C, onions are a good source of dietary fiber, and folic acid. Onions also help to improve the functions of the digestive system; because it releases the digestive juices and cures any problem related to digestion.",
+      price: 50,
+      countInStock: 10,
+    },
+    {
+      name: "Bread",
+      imageUrl:
+        "https://cflare.shop.bigbazaar.com/cdn-cgi/image/width=450/media/product/19/571/10-wide_zoom.jpg",
+      description:
+        "Preservative free bread",
+      price: 7,
+      countInStock: 5,
+    },
+    {
+      name: "Milk",
+      imageUrl:
+        "https://cflare.shop.bigbazaar.com/cdn-cgi/image/width=450/media/product/23/87/1-wide_zoom.jpg",
+      description:
+        "Milk ",
+      price: 5,
+      countInStock: 25,
+    },
+    {
+      name: "Egg",
+      imageUrl:
+        "https://cflare.shop.bigbazaar.com/cdn-cgi/image/width=450/media/product/79/973/11_1-wide_zoom.jpg",
+      description:
+        "Full of proteins and energy",
+      price: 3,
+      countInStock: 4,
+    },
+    {
+      name: "Milk",
+      imageUrl:
+        "https://cflare.shop.bigbazaar.com/cdn-cgi/image/width=450/media/product/23/87/1-wide_zoom.jpg",
+      description:
+        "Milk ",
+      price: 5,
+      countInStock: 25,
+    },
+  ];
+  
+  module.exports = products;
